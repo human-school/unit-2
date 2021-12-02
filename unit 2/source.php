b824a6e5ff71f90bccf63aea4244a882df2f9649
this-is-for-a-school-thing <matthew.j.cummings123@gmail.com>
2021-12-02 23:56:22 +1100

    a lot of changes

--- unit 2/source.php
+++ unit 2/source.php
@@ -13,6 +13,32 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <html><!-- InstanceBegin template="/Templates/login_template.dwt" codeOutsideHTMLIsLocked="false" -->
 	<link rel='icon' href='/images/iss.ico' type='image/x-icon'/ >
 <head>
+	<script>
+
+    var toggleimage=new Array("/images/1.jpg","/images/2.jpg")
+
+    //do not edit the variables below
+    var image_1=new Image()
+    var image_2=new Image()
+    image_1.src=toggleimage[0]
+    image_2.src=toggleimage[1]
+    var i_image=0
+
+    function testloading() {
+        isloaded=true
+    }
+
+    function toggle() {
+        if (isloaded) {
+            document.togglepicture.src=toggleimage[i_image]
+        }
+        i_image++
+        if (i_image>1) {i_image=0}
+    }
+    onload=testloading
+    </script>
+
+
 <meta charset="UTF-8">
 <title>source</title>
 <!-- InstanceParam name="main" type="boolean" value="true" -->
@@ -23,7 +49,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   <tbody>
     <tr>
       <td width="%" height=""><a href="logout.php"><img src="images/ERROR%20404VACATION%20NOT%20FOUND%20(200%20x%20200%20px).png" width="150" height="144" alt=""/></a></td>
-      <td width="93%"><a href="https://github.com/this-is-for-a-school-thing/unit-2"><img src="images/source%20code.png" width="1391" height="144" alt=""/></a></td>
+      <td width="93%"><a href="https://github.com/this-is-for-a-school-thing/unit-2"><img src="images/source%20code.png" width="100%" height="144" alt=""/></a></td>
     </tr>
     <tr>
       <td height="742" valign="top"><p><a href="logout.php"><img src="images/Sign%20Out%20Of%20Your%20Account-1.png" width="150" height="40" alt=""/></a></p>
@@ -56,7 +82,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 				  <p>(put cool *things* here)&nbsp;</p>
 				  <p>if you dumb make a account here. --> <a href="register.php">Sign up</a>.</p>
 			    
-		      <img src="videos/rick-ashley-dance.gif" width="200" height="135" alt=""/></tr>
+	            <p><img src="videos/rick-ashley-dance.gif" width="200" height="135" alt=""/></p>
+	            <p>&nbsp;   
+			<a href="javascript:toggle()"><img name="togglepicture" src="/images/2.jpg" border="0"></a>
+		
+	
+</p>	            </tr>
           </tbody>
         </table>
         <p>&nbsp;</p>
